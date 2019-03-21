11c89f24e2b3b892fbbaeb983594428a56667a63
Lê Phúc Lâm <39263136+lephuclam97@users.noreply.github.com>
2019-03-21 19:07:10 +0700

    add UserPage

--- source/cms-management/src/pages/SettingPage/SettingPage.js
+++ source/cms-management/src/pages/SettingPage/SettingPage.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SettingPage extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>This is Setting Pages</h1>
       </div>
     );
