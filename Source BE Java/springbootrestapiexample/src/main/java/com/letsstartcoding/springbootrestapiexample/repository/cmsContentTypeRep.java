7db639c4750c366bacdb3253032f28415b5ba334
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-03-31 16:30:09 +0700

    Code error

--- Source BE Java/springbootrestapiexample/src/main/java/com/letsstartcoding/springbootrestapiexample/repository/cmsContentTypeRep.java
+++
@@ -1,9 +0,0 @@
-package com.letsstartcoding.springbootrestapiexample.repository;
-
-import com.letsstartcoding.springbootrestapiexample.model.cmsContentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface cmsContentTypeRep extends JpaRepository<cmsContentType, Long> {
-
-}
