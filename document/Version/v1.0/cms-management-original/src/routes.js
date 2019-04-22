a183e6fc109d1ce4bde6cb4f4a56762ed645d76f
lephuclam97 <lamboyboy20@gmail.com>
2019-04-23 00:11:20 +0700

    add version

---
+++ document/Version/v1.0/cms-management-original/src/routes.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/display-name */
+import React from "react";
+import ContentPage from "./pages/ContentPage/ContentPage";
+import MediaPage from "./pages/MediaPage/MediaPage";
+import SettingPage from "./pages/SettingPage/SettingPage";
+import UserPage from "./pages/UserPage/UserPage";
+import FormPage from "./pages/FormPage/FormPage";
+import PackagesPage from "./pages/PackagesPage/PackagesPage";
+
+const routes = [
+  {
+    path: "/",
+    main: () => <ContentPage />
+  },
+  {
+    path: "/media",
+    main: () => <MediaPage />
+  },
+  {
+    path: "/setting",
+    main: () => <SettingPage />
+  },
+  {
+    path: "/packages",
+    main: () => <PackagesPage />
+  },
+  {
+    path: "/users",
+    main: () => <UserPage />
+  },
+  {
+    path: "/forms",
+    main: () => <FormPage />
+  },
+  {
+    path: "/packages",
+    main: () => <PackagesPage/>
+  }
+];
+
+export default routes;
